8b96a7678f8fbae87b6d07746c41f7d9b1f6d111
Nadine Chernova <nchernova2702@gmail.com>
2025-05-13 17:18:35 -0700

    Refactored RayGun script

--- Assets/Scripts/RayGun.cs
+++ Assets/Scripts/RayGun.cs
@@ -7,14 +7,19 @@ using UnityEngine.InputSystem;
 // (no need for code-based binding)
 public class RayGun : MonoBehaviour
 {
-    public LineRenderer laserPrefab;
-    public GameObject rayImpactPrefab;
-    public Transform shootingPoint;
-    public float maxDistance = 5f;
-    public float laserDuration = 0.2f;
-    public AudioSource shootAudio;
-    public AudioClip shootClip;
-    public LayerMask hitMask;
+    [Header("Laser & Impact")] [SerializeField]
+    private LineRenderer laserPrefab;
+
+    [SerializeField] private GameObject rayImpactPrefab;
+    [SerializeField] private float laserDuration = 0.2f;
+
+    [Header("Shooting")] [SerializeField] private Transform shootingPoint;
+    [SerializeField] private float maxDistance = 5f;
+    [SerializeField] private float damage = 10f; // damage per shot
+    [SerializeField] private LayerMask hitMask;
+
+    [Header("Audio")] [SerializeField] private AudioSource shootAudio;
+    [SerializeField] private AudioClip shootClip;
 
     public void ShootTrigger(InputAction.CallbackContext context)
     {
@@ -36,35 +41,43 @@ public class RayGun : MonoBehaviour
         // if hit something, laser ends at the hit point, or goes max distance forward
         Vector3 targetPoint = hit ? hitInfo.point : shootingPoint.position + shootingPoint.forward * maxDistance;
 
-
         // If hit smth, react
         if (hit)
         {
-            // if ray hits gameobj with IDamageable, call TakeDamage()
-            IDamageable damageable = hitInfo.transform.GetComponentInParent<IDamageable>();
-            if (damageable != null)
-            {
-                Debug.Log("RayGun: Hit a damageable object: " + hitInfo.transform.name);
-                damageable.TakeDamage(10f, hitInfo.point, hitInfo.normal, gameObject);
-            }
-            // hits not damageable(walls)
-            else if (rayImpactPrefab)
-            {
-                Debug.Log("RayGun: Hit a non-damageable object: " + hitInfo.transform.name);
-                Quaternion rotation = Quaternion.LookRotation(-hitInfo.normal);
-                GameObject impact = Instantiate(rayImpactPrefab, hitInfo.point, rotation);
-                Destroy(impact, 1f);
-            }
+            HandleHit(hitInfo);
         }
 
         if (laserPrefab)
         {
-            LineRenderer lr = Instantiate(laserPrefab);
-            lr.positionCount = 2;
-            lr.SetPosition(0, shootingPoint.position);
-            lr.SetPosition(1, targetPoint);
-            Destroy(lr.gameObject, laserDuration);
+            DrawLaser(shootingPoint.position, targetPoint);
         }
+    }
 
+    private void HandleHit(RaycastHit hitInfo)
+    {
+        // if ray hits gameobj with IDamageable, call TakeDamage()
+        IDamageable damageable = hitInfo.transform.GetComponentInParent<IDamageable>();
+        if (damageable != null)
+        {
+            Debug.Log("RayGun: Hit a damageable object: " + hitInfo.transform.name);
+            damageable.TakeDamage(damage, hitInfo.point, hitInfo.normal, gameObject);
+        }
+        // hits not damageable(walls)
+        else if (rayImpactPrefab)
+        {
+            Debug.Log("RayGun: Hit a non-damageable object: " + hitInfo.transform.name);
+            Quaternion rotation = Quaternion.LookRotation(-hitInfo.normal);
+            GameObject impact = Instantiate(rayImpactPrefab, hitInfo.point, rotation);
+            Destroy(impact, 1f);
+        }
+    }
+
+    private void DrawLaser(Vector3 start, Vector3 end)
+    {   
+        LineRenderer lr = Instantiate(laserPrefab);
+        lr.positionCount = 2;
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+        Destroy(lr.gameObject, laserDuration);
     }
 }
